62bb11d6d67796ea73f349825d2e70308a7d6cbf
ozabell <kidlatmc@gmail.com>
2020-09-11 13:50:15 -0700

    added while loop to repeat as long as the user wants

--- lab1.cpp
+++ lab1.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 const int ARR_LENGTH = 10;
+const char YES = 'y';
 
 void getInput(int max, int arr[]);
 int findMin(int arr[]);
@@ -16,24 +17,29 @@ int main()
 
   int nums[ARR_LENGTH] = {0};
   int max;
+  char again = YES;
 
   cout <<  endl << "- Welcome to Lab1 - " << endl;
 
-  cout << "Please enter a max value: ";
-  cin >> max;
+  while (again == YES) {
+    cout << "Please enter a max value: ";
+    cin >> max;
 
-  getInput(max, nums);
+    getInput(max, nums);
 
-  cout << endl << endl;
+    cout << endl << endl;
 
-  cout << "Printing array.... " << endl;
-  for(int i = 0; i < ARR_LENGTH; i++) {
-    cout << "#" << i;
-    cout << " = " << nums[i] << " ";
-    cout << endl;
+    cout << "Printing array.... " << endl;
+    for(int i = 0; i < ARR_LENGTH; i++) {
+      cout << "#" << i;
+      cout << " = " << nums[i] << " ";
+      cout << endl;
+    }
+
+    cout << "Minimum value: " << findMin(nums) << endl;
+    cout << "Again? (y/n): ";
+    cin >> again; 
   }
-
-  cout << "Minimum value: " << findMin(nums) << endl;
 
   cout << endl << "- End of Lab1 Program - " << endl;
 
