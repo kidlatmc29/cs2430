5477bc80630fab4eabf3c482ccfc1773eb8f24ef
ozabell <kidlatmc@gmail.com>
2020-09-11 08:29:29 -0700

    added main method to lab1

--- lab1.cpp
+++ lab1.cpp
@@ -1,1 +1,16 @@
-// Ovalles, Isabel 
+// Ovalles, Isabel
+// lab1.cpp
+// 09-11/2020
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Welcome message" << endl;
+
+  cout << endl << "End of Lab1 Program" << endl;
+  
+  return 0;
+}
