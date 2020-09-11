4756ccab414a4f3155428cddc48eb55af17c3ce9
ozabell <kidlatmc@gmail.com>
2020-09-11 08:35:42 -0700

    added lab1 instructions

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,6 @@
 // Ovalles, Isabel
 // lab1.cpp
-// 09-11/2020
+// 09-11-2020
 
 #include <iostream>
 
@@ -10,7 +10,33 @@ int main()
 {
   cout << "Welcome message" << endl;
 
+  // 3.)
+  // Write the code needed to accept interger input from the keyboard
+  // Unit 8 EVEN numbers greater than or equal to 25 have been stored
+  // In a STATIC array
+
+  // 4.)
+  //Output the values along with the array index for each
+
+  // 5.)
+  // Put the code from 3.) into a function and call the function from main
+  // with a PARAMETER that determins the min value that will be stored.
+  // Replacing 25. Accept inter input from the keyboard to determine the int main(int argc, char const *argv[]) {
+  // value.
+
+  // 6.)
+  // Write a function that returns the min value stored in the array and call the fxn
+  // from main, printing the value returned in main.
+
+  // 7.)
+  // Add the code that will allow the input and serach to be repated as many
+  // times as the user desiers
+
+    return 0;
+  }
+
+
   cout << endl << "End of Lab1 Program" << endl;
-  
+
   return 0;
 }
