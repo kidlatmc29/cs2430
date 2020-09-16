4461d8c43a60a2d040a053650e4d47066400a54d
ozabell <kidlatmc@gmail.com>
2020-09-16 12:20:15 -0700

    setting up lab2 with comments for instructions

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,3 +1,28 @@
 // Ovalles, Isabel 
 // lab2.cpp
 // 09-18-2020
+
+#include <iostream> 
+using namespace std; 
+
+class reverseStack {
+  public:
+    //push()
+    //pop()
+    //print() 
+  private:
+    // dynamically allocated int array
+    // size of the array
+    // elements in the array
+    // isFull() fxn
+};
+
+int main()
+{
+  /**
+   * Allow the user to push or pop as many elements as they wish. 
+   * When the stack is full OR when the user wants to stop, 
+   * print all the elements in reverseStack and end the program.
+   **/
+  return 0;
+}
