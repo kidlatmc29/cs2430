a04692ee9e0885302108c18a9e8413fef0f0306c
ozabell <kidlatmc@gmail.com>
2020-09-18 06:27:02 -0700

    creating while loop to pop push or stop

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -5,29 +5,39 @@
 #include <iostream> 
 using namespace std; 
 
-//static char PUSH = 'p';
-//static char POP = 'o';
-//static char STOP = 's';
+static char PUSH = 'p';
+static char POP = 'o';
+static char STOP = 's';
 typedef int elementType; 
 
 int main()
 {
   int size;
   int maxSize; 
-  elementType* array; 
+  elementType* reverseStack; 
   /**
    * Allow the user to push or pop as many elements as they wish. 
    * When the stack is full OR when the user wants to stop, 
    * print all the elements in reverseStack and end the program.
    **/
 
-  int input; 
+  int arrSize; 
+  char input; 
 
   cout << endl << "Welcome to Lab 2!" << endl;
   cout << "Please enter an array size: ";
-  cin >> input; 
+  cin >> arrSize; 
 
-  cout << "";
+  reverseStack = new int[arrSize];
+  maxSize = arrSize; 
+
+  cout << "Do you want to push (p), pop (o), or stop (s)?: ";
+  cin >> input; 
+  while(input != STOP) {
+    cout << "you selected: " << input << endl;
+    cout << "Do you want to push (p), pop (o), or stop (s)?: ";
+    cin >> input; 
+  }
   
   cout << endl << "End of Lab2" << endl;
   return 0;
