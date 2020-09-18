fc368db8e3953902b63652d08014eaada463673f
ozabell <kidlatmc@gmail.com>
2020-09-18 13:38:22 -0700

    doing some formating and commenting

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -2,9 +2,16 @@
 // lab2.cpp
 // 09-18-2020
 
-// DESCRIPTION: 
+// DESCRIPTION: Lab2 asks the user for a length of an array and creates it. 
+// The user then is asked to select a choice from a menu, either pushing, 
+// popping, stopping. The choice is exectuted and the user can continue until
+// it wants to stop or the stack is full. Then the stack is printed out. 
+
 // ASSUMPTION: None. 
+
 // SOURCES: 
+// 
+
 #include <iostream> 
 using namespace std; 
 
@@ -14,14 +21,19 @@ const char STOP = 's';
 typedef int elementType; 
 
 char getUserInput(); 
-// Asks user for their choice from the menu, returns the choice
+// Asks user for their choice from the menu, returns their choice.
 // PRE: None.
 // POST: None. 
 
 void push(elementType *stack, elementType value, int& size, int maxSize);
 // Adds one new element to the top of the reverseStack, returns new size
-// PRE: value must be type elementType in order to be pushed
-// POST:
+// PRE: Value must be type elementType in order to be pushed.
+// POST: Adds 1 to size. 
+
+elementType pop(elementType *stack, int& size);
+// Removes the top element of the stack, returns the value that was popped.
+// PRE: The stack must have a size greater than 0.
+// POST: Subtracts 1 from size. 
 
 bool isFull(int size, int maxSize);
 // Returns true if stack is full
@@ -29,14 +41,10 @@ bool isFull(int size, int maxSize);
 // POST: None.
 
 void print(elementType *stack, int maxSize);
-// Prints out all the elements in the stack in reverse order
+// Prints out all the elements in the stack in reverse order. If stack is empty
+// it will print nothing. 
 // PRE: None. 
 // POST: None. 
-
-elementType pop(elementType *stack, int& size);
-// 
-// PRE:
-// POST: 
 
 int main()
 {
@@ -60,11 +68,9 @@ int main()
   reverseStack = new elementType[maxSize];
 
   while((choice != STOP) && !(isFull(size, maxSize))) {
-    
     while((choice != PUSH) && (choice != POP) && (choice != STOP)) {
       choice = getUserInput();
     }
-    
     switch(choice)
     {
       case PUSH :
@@ -77,8 +83,11 @@ int main()
         } else {
           cout << value << " is smaller than the current top." << endl;
         }
+        cout << endl;
 
+        if(size < maxSize) {
         choice = getUserInput(); 
+        }
         break; 
       }
       case POP :
@@ -88,14 +97,15 @@ int main()
       } else {
         cout << "reverseStack is empty! " << endl;
       }
-       choice = getUserInput();
-       break;
+        cout << endl;
+        choice = getUserInput();
+        break;
       }
     }
   }
 
   print(reverseStack, size);
-  cout << endl << "End of Lab2" << endl << endl;
+  cout << "End of Lab2" << endl << endl;
 
   delete[] reverseStack;
   return 0;
@@ -118,6 +128,14 @@ void push(elementType *stack, elementType value, int& size, int maxSize)
   cout << "Pushed " << value << endl;
 }
 
+elementType pop(elementType *stack, int& size)
+{
+    elementType poppedVal = stack[size - 1];
+    stack[size - 1] = 0; 
+    size--;
+    return poppedVal; 
+}
+
 bool isFull(int size, int maxSize)
 {
   return size >= maxSize; 
@@ -133,12 +151,4 @@ void print(elementType *stack, int size)
     cout << stack[index] << ", "; 
     }
   }
-}
-
-elementType pop(elementType *stack, int& size)
-{
-    elementType poppedVal = stack[size - 1];
-    stack[size - 1] = 0; 
-    size--;
-    return poppedVal; 
 }
