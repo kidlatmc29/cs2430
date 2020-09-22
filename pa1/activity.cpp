21a7ca9e1472449c36fec4ce5deddebcfa8339e7
ozabell <kidlatmc@gmail.com>
2020-09-22 14:58:38 -0700

    figured out conidtional was wrong in enqueue

--- pa1/activity.cpp
+++ pa1/activity.cpp
@@ -2,79 +2,73 @@
 // activity.cpp
 // 9-28-2020
 
-#include "activity.h" 
+#include "activity.h"
 
 ActivityList::ActivityList()
 {
-  cout << "in default constructor" << endl;
-  head = nullptr; 
+  head = nullptr;
 }
 
 ActivityList::ActivityNode::ActivityNode(string n, float p, float r, int pri)
 {
   name = n;
   price = p;
-  rating = r; 
+  rating = r;
   pri = priority;
 }
 
 ActivityList::~ActivityList()
 {
-  ActivityNode *nPtr = head; 
-  ActivityNode *nextPtr; 
-  cout << "Deallocating ActivityList...." << endl;
+  ActivityNode *nPtr = head;
+  ActivityNode *nextPtr;
   while(nPtr) {
-    nextPtr = nPtr->next; 
-    delete nPtr; 
-    nPtr = nextPtr; 
+    nextPtr = nPtr->next;
+    delete nPtr;
+    nPtr = nextPtr;
   }
-  head = nullptr; 
+  head = nullptr;
 }
 
 void ActivityList::enqueue(string n, float p, float r, int pri)
 {
   cout << "in enqueue fxn" << endl;
   ActivityNode *newNode = new ActivityNode(n, p, r, pri);
-  ActivityNode *nPtr; 
+  ActivityNode *nPtr;
+  size++;
 
-  if(isEmpty() || head->priority >= newNode->priority) {
-    cout << "list is empty so setting head as newNode" << endl;
-    head = newNode; 
-    size++; 
-    newNode->next = nullptr; 
+  if((isEmpty()) || head->priority >= newNode->priority) {
+    newNode->next = head;
+    head = newNode;
   } else {
-    nPtr = head; 
-    while((nPtr->next && nPtr->next->priority < newNode->priority)) {
-      nPtr = nPtr->next; 
+    nPtr = head;
+    while(nPtr->next && (nPtr->next->priority < newNode->priority)) {
+      nPtr = nPtr->next;
     }
     newNode->next = nPtr->next;
-    nPtr->next = newNode; 
+    nPtr->next = newNode;
   }
 }
 
 void ActivityList::printList()
 {
-  int elements  = 1;
   ActivityNode *nPtr = head;
   cout << "Printing ActivityList: " << endl;
-  
+
   if (isEmpty()) {
     cout << "the activity list is empty! " << endl;
-  } else if(size == 1) {
-    cout << elements <<  ". [" << nPtr->priority << "] " << nPtr->name << " - " 
-         << nPtr-> price << " - " << nPtr->rating << " stars" << endl;
-  } else { 
+  } else if (!head->next){
+    cout << " there is only one element in the list... printing" << endl;
+    cout << nPtr->name << " " << nPtr->priority << endl;
+  } else {
+    cout << "list is not empty so I stuff to print";
       while(nPtr->next) {
-        cout << elements << ". [" << nPtr->priority << "] " << nPtr->name
-             << " - "  << nPtr-> price << " - " << nPtr->rating << " stars" 
-             << endl;
-        elements++;
-        nPtr = nPtr->next; 
+        cout << nPtr->name << " " << nPtr->priority << endl;
+      nPtr = nPtr->next; 
     }
   }
 }
 
 bool ActivityList::isEmpty()
 {
-  return head; 
+  return !(head);
 }
--- pa1/pa1.cpp
+++ pa1/pa1.cpp
@@ -9,50 +9,25 @@
 
 using namespace std; 
 
-const string FILE_NAME = "activity.csv";
-const char delimeter = ','; 
+// const string FILE_NAME = "activity.csv";
+// const char delimeter = ','; 
 
 void readFile(ActivityList &myList);
 
 int main()
 {
-  cout << "PA1 Start! " << endl;
+  cout << "PA1 Starting... " << endl;
   ActivityList myList; 
+  
   cout << "Adding Pike Place Market into myList...." << endl;
   myList.enqueue("Pike Place Market", 0, 4.6, 1);
   myList.printList();
-  // read in activities from file, adding activity nodes to the list
-    // readFile(myList);
 
-
-
-  // testing magic
-
-  cout << "PA1 Ending" << endl;
-  // clean up
+  cout << "PA1 Ending..." << endl;
   return 0;
 }
 
 // read file fxn 
 void readFile(ActivityList &list)
 {
-  ifstream input; 
-  string line; 
-  stringstream ss;
-  bool failed = false; 
-
-  cout << "Reading in file " << FILE_NAME << "..." << endl;
-  input.open(FILE_NAME);
-
-  if(!failed) {
-    while(getline(input, line)) {
-      for(int row = 1; row <= 4; row++) {
-        
-      }
-      // get the whole line, then split on the ',' to get indiviual values 
-      // call enqueue passing in the parameters to create the activity node
-    }
-  }
-
-  input.close();
 }
