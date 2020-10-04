c15dbcd72f214b502c6cf3fa6f826e80b42b4e56
ozabell <kidlatmc@gmail.com>
2020-10-03 17:27:48 -0700

    working on driver first

--- pa2/pa2.cpp
+++ pa2/pa2.cpp
@@ -7,14 +7,17 @@
 // SOURCES:
 
 #include <iostream> 
+#include "recursiveQueue.h"
 
 using namespace std; 
 
 int main()
 {
   int input; 
+  cout << endl << endl;
   cout << "Welcome to PA 2. Please enter a positive integer: ";
   cin >> input; 
-  
+
+  cout << "End of PA 2." << endl << endl;
   return 0; 
 }
