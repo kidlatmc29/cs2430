9f6f27da7093a1fb513b8f8d9342049d7722da5d
ozabell <kidlatmc@gmail.com>
2020-10-09 13:33:21 -0700

    starting lab4

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -6,7 +6,57 @@
 
 using namespace std;
 
+typedef int treeElement; 
+
+struct BNode {
+  treeElement value;
+  BNode *left; 
+  BNode *right; 
+};
+
+bool insert (BNode* &n, treeElement k);
+
+bool contains(BNode* root, treeElement k);
+
+int sumOfBST(BNode* root);
+
+void clearTree(BNode* root);
+
 int main()
 {
+  int numOfKeys = 0;
+  BNode *root = nullptr; 
+
+  cout << endl << "Start of Lab 4" << endl;
+  cout << "How many keys do you want to add to the BST?: ";
+  cin >> numOfKeys; 
+
+
+  cout << endl << "End of Lab 4" << endl;
   return 0;
+}
+
+bool insert(BNode* &n, treeElement k)
+{
+
+}
+
+bool contains(BNode* root, treeElement k)
+{
+
+}
+
+void printSmallerK(BNode* root, treeElement k)
+{
+
+}
+
+int sumOfBST(BNode* root)
+{
+
+}
+
+void clearTree(BNode* root)
+{
+
 }
