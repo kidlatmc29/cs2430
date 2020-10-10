2a42e13223ef4b36d5251b2b763aa9a56e700d02
ozabell <kidlatmc@gmail.com>
2020-10-09 17:34:33 -0700

    creating main fxn to test insert

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -18,45 +18,94 @@ bool insert (BNode* &n, treeElement k);
 
 bool contains(BNode* root, treeElement k);
 
-int sumOfBST(BNode* root);
+int sumOfBST(BNode* n);
 
-void clearTree(BNode* root);
+void printSmallerK(BNode* n, treeElement k);
+
+void clearTree(BNode* n);
+
+void print(BNode* n);
 
 int main()
 {
   int numOfKeys = 0;
+  int count; 
+  treeElement val; 
   BNode *root = nullptr; 
 
   cout << endl << "Start of Lab 4" << endl;
   cout << "How many keys do you want to add to the BST?: ";
   cin >> numOfKeys; 
 
+  while(count != numOfKeys) {
+    cout << "Please enter a value: ";
+    cin >> val;
+    if(insert(root, val)) {
+      count++;
+    } else {
+      cout << endl << "Please enter a unique vaue next time." << endl;
+    }
+  }
+  
 
-  cout << endl << "End of Lab 4" << endl;
+  print(root);
+
+  cout << endl << "End of Lab 4" << endl << endl;
   return 0;
 }
 
 bool insert(BNode* &n, treeElement k)
 {
-
+  int unique = false;
+  if(!contains(n, k)) {
+    unique = true;
+    if(n == nullptr) {
+      n = new BNode; 
+      n->value = k;
+      n->right = nullptr;
+      n->left = nullptr; 
+    } else if(n->value > k) {
+      insert(n->left, k);
+    } else {
+      insert(n->right, k);
+    }
+  }
+  return unique;
 }
 
-bool contains(BNode* root, treeElement k)
+bool contains(BNode* n, treeElement k)
+{
+  if(n == nullptr) {
+    return false;
+  } else if (n->value == k) {
+    return true;
+  } else if (n->value > k) {
+    return contains(n->left, k);
+  } else {
+    return contains(n->right, k);
+  }
+}
+
+void printSmallerK(BNode* n, treeElement k)
 {
 
 }
 
-void printSmallerK(BNode* root, treeElement k)
+int sumOfBST(BNode* n)
 {
-
+  return -1; 
 }
 
-int sumOfBST(BNode* root)
+void clearTree(BNode* n)
 {
-
+  // post order traversal 
 }
 
-void clearTree(BNode* root)
+void print(BNode* n)
 {
-
+  if(n != nullptr) {
+    cout << n->value << " ";
+    print(n->left);
+    print(n->right);
+  }
 }
