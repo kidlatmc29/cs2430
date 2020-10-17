2fc52bfda99f4d01cca42915e4fd5afbb1dbad79
ozabell <kidlatmc@gmail.com>
2020-10-17 14:00:31 -0700

    working on pa3 adding boardgame file

--- pa3/pa3.cpp
+++ pa3/pa3.cpp
@@ -1,3 +1,23 @@
 // Ovalles, Isabel 
 // pa3.cpp
 // 10-21-2020
+
+// DESCRIPTION: 
+
+#include <iostream> 
+#include <fstream> 
+#include <sstream>
+
+#include <gamebst.h> 
+
+using namespace std; 
+
+const string FILE_NAME = "boardgames.csv";
+const char DELIMITER = ','; 
+
+int fileRead();
+
+int main()
+{
+  return 0;
+}
