a498e101a0544c440f08381fbf0a56b21bf2e699
ozabell <kidlatmc@gmail.com>
2020-10-29 14:16:16 -0700

    added barebone main

--- pa4/pa4.cpp
+++ pa4/pa4.cpp
@@ -11,5 +11,25 @@ using namespace std;
 
 int main()
 {
+  int numOfVals; 
+  string input; 
+
+  cout << endl  << "Welcome to PA 4" << endl;
+  cout << "How many values do you wnt to add to the heap? :";
+  cin >> numOfVals;
+  
+  for(int i = 0; i < numOfVals; i++) {
+    cout << "Enter a word you want to add: ";
+    cin >> input;
+    // call insert fxn on heap in here
+    cout << "Adding " << input << " ....";
+  }
+
+  cout << "What value do you want to print the children of? :";
+  cin >> input; 
+  // call printing fxn or whatever
+
+  // call emptying heap fxn here 
+  
   return 0;
 }
